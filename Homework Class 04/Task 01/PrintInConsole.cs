e540476098af4a636027c4c841f58a1b1d0965b6
kristijansofronijoski <kristijansofronijoski@gmail.com>
2025-05-10 13:59:25 +0200

    Homework 3

---
+++ Homework Class 04/Task 01/PrintInConsole.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Task_01
+{
+    public class PrintInConsole<T>
+    {
+        public void Print(List<T> items)
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void PrintCollection(List <T> items)
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Homework Class 04/Task 01/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+ Task 1
+Create class PrintInConsole that will have multiple methods to print in console. Print(), PrintCollection().
+Make this methods to be valid for more that one type and reuse the implenetation (hint: make them generic);
+ */
+
+using Task_01;
+
+
+List<int> numbers = new List<int> (){ 1, 2, 3, 4, 5 };
+List<string> strings = new List<string>() { "Hello", "World" };
+
+//PrintInConsole<int>.Print(numbers);
+//PrintInConsole<string>.Print(strings);
+//PrintInConsole<int>.PrintCollection(numbers);
+//PrintInConsole<string>.PrintCollection(strings);
+
+PrintInConsole<int> intHelper = new PrintInConsole<int>();
+PrintInConsole<string> stringHelper = new PrintInConsole<string>();
+intHelper.Print(numbers);
+stringHelper.Print(strings);
